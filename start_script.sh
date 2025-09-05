259f4705e437d2996f8b8d9eb397c7d531b52c76
Suhyeon Park <ssantta999@gmail.com>
2025-09-05 16:42:19 +0900

    백엔드까지 완료

---
+++ start_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+# 텔레포트 데몬으로 시작
+docker-compose up -d teleport-daemon
+
+# 텔레포트 서비스 HTTP(3080) 준비 대기 (curl 또는 nc 등 활용)
+until curl -s http://localhost:3080 > /dev/null; do
+  echo "Waiting for teleport-daemon to be ready..."
+  sleep 2
+done
+
+# 텔레포트 역할 생성
+if ! docker exec teleport-daemon tctl get role teleport-event-handler > /dev/null 2>&1; then
+  docker exec teleport-daemon tctl create -f event-listen/plugin/teleport-event-handler-role.yaml
+  echo "Role teleport-event-handler created."
+else
+  echo "Role teleport-event-handler already exists."
+fi
+
+# 텔레포트에 bot 삭제 및 토큰 발급
+docker exec teleport-daemon tctl bots rm jarvis-bot || true
+JOIN_TOKEN=$(docker exec teleport-daemon tctl bots add jarvis-bot --roles=editor,teleport-event-handler --ttl=5m | grep 'The bot token: ' | awk '{print $4}')
+
+if [ -z "$JOIN_TOKEN" ]; then
+  echo "Error: Failed to generate or extract bot join token."
+  exit 1
+fi
+
+echo "JOIN_TOKEN=$JOIN_TOKEN" > .env
+
+# 백엔드 코드에서 사용되는 마스터 유저 생성은 사용자가 직접하면 된다.
+
+# 백엔드 서비스 시작
+docker-compose up -d backend
+
+# ELK + plubin 서비스 시작
+docker-compose up -d logstash elasticsearch kibana
+
+# ELK 서비스 준비 대기
+until curl -s http://localhost:5601 > /dev/null; do
+  echo "Waiting for Kibana to be ready..."
+  sleep 2
+done
+
+echo "ELK services are up and running."
+
+# 프론트 서비스 시작

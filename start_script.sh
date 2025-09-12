c9f4fdebcbb80304cee0fc3017633e1da21c2285
Suhyeon Park <ssantta999@gmail.com>
2025-09-12 18:43:56 +0900

    updating

--- start_script.sh
+++ start_script.sh
@@ -2,7 +2,7 @@
 set -e
 
 export $(grep -v '^#' .env | xargs)
-envsubst < /teleport-daemon/teleport.yaml.template > /teleport-daemon/teleport.yaml
+envsubst < /teleport-daemon/config/teleport.yaml.template > /teleport-daemon/config/teleport.yaml
 
 # 텔레포트 데몬으로 시작
 docker-compose up -d teleport-daemon

22e118ccbc1d76458c1ea5251b6f703fbe97a157
Suhyeon Park <ssantta999@gmail.com>
2025-09-15 16:48:38 +0900

    new

--- start_script.sh
+++ start_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-envsubst < ./teleport-daemon/config/teleport.yaml.template > ./teleport-daemon/config/teleport.yaml
-//일단 손수 수정하는걸로 나중에 수정
-
 # 텔레포트 데몬으로 시작
 docker-compose up -d teleport-daemon
